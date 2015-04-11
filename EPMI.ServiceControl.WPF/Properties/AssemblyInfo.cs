74e666bfe81be138597c673a7e35db2c7926ff7b
Keith Kikta <keith.kikta@epmintelligence.com>
2015-04-11 03:32:15 -0500

    Fixed uninstall issue

--- EPMI.ServiceControl.WPF/Properties/AssemblyInfo.cs
+++ EPMI.ServiceControl.WPF/Properties/AssemblyInfo.cs
@@ -77,4 +77,4 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ""AS IS"" AN
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.3.2")]
